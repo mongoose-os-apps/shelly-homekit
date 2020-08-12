3e3e1d449f495ad30e78f2a7fdb6b56a15609be5
Andrew Blackburn <andyblac@icloud.com>
2020-08-12 17:11:16 +0100

    refactor code to pass variables to functions, rather than using global variables. add script option's '-h, -f -c'   -f, --flash         Flash the lastest available firmware.   -c, --check-only    only check for updates.   -h, --help          This help text.

--- tools/flash_shellys.sh
+++ tools/flash_shellys.sh
@@ -22,7 +22,12 @@
 #  or any other firmware please follow instructions here: 
 #  https://github.com/mongoose-os-apps/shelly-homekit/blob/master/README.md
 #  
-#  usage: ./flash_shellys.sh
+#  -f, --flash         Flash the lastest available firmware.
+#  -c, --check-only    only check for updates.
+#  -h, --help          This help text.
+# 
+#  usage: ./flash_shellys.sh -f
+
 
 function install_brew {
   echo -e '\033[1mInstalling brew...\033[0m'
@@ -58,25 +63,25 @@ function convert_to_integer {
 }
 
 function download {
-  curl -L -o shelly-flash.zip "$@"
+  curl -L -o shelly-flash.zip "$1"
 }
 
-function flash {
-  download "$@"
+function write_flash {
+  download "$3"
   if [ -f shelly-flash.zip ];then
     while true; do
-      read -p "Are you sure you want to flash $device to firmware version $lfw ?" yn
+      read -p "Are you sure you want to flash $1 to firmware version $2 ? " yn
       case $yn in
-        [Yy]* )  echo "Now Flashing.."; curl -F file=shelly-flash.zip http://$device/update; break;;
+        [Yy]* )  echo "Now Flashing.."; curl -F file=shelly-flash.zip http://$1/update; break;;
         [Nn]* ) echo "Skipping..";break;;
         * ) echo "Please answer yes or no.";;
       esac
     done
   fi
-  echo "waiting for $device to reboot"
+  echo "waiting for $1 to reboot"
   sleep 10
-  if [ $(curl -qsS -m 5 http://$device/rpc/Sys.GetInfo | jq -r .fw_version) == $lfw ];then
-    echo "Successfully flashed $device to $lfw"
+  if [ $(curl -qsS -m 5 http://$1/rpc/Sys.GetInfo | jq -r .fw_version) == $2 ];then
+    echo "Successfully flashed $1 to $2"
   else
     echo "Flash failed!!!"
   fi
@@ -84,68 +89,99 @@ function flash {
   read -p "Press enter to continue"
 }
 
-echo -e '\033[1mScanning for Shelly devices...\033[0m'
-release_info=$(curl -qsS -m 5 https://api.github.com/repos/mongoose-os-apps/shelly-homekit/releases/latest)
-lfw=$(echo "$release_info" | jq -r .tag_name)
+function device-scan {
+  echo -e '\033[1mScanning for Shelly devices...\033[0m'
+  release_info=$(curl -qsS -m 5 https://api.github.com/repos/mongoose-os-apps/shelly-homekit/releases/latest)
+  lfw=$(echo "$release_info" | jq -r .tag_name)
 
-for device in $(timeout 2 dns-sd -B _hap . | awk '/shelly/ {print $7}'); do
-  device="$device.local"
-  flash=null
-  clear
-  if [[ $(curl -qsS -m 5 http://$device/rpc/Shelly.GetInfo) == "Not Found" ]]; then
-    continue
-  fi
+  for device in $(timeout 2 dns-sd -B _hap . | awk '/shelly/ {print $7}'); do
+    device="$device.local"
+    flash=null
+    if [[ $(curl -qsS -m 5 http://$device/rpc/Shelly.GetInfo) == "Not Found" ]]; then
+      continue
+    fi
 
-  info=$(curl -qsS -m 5 http://$device/rpc/Shelly.GetInfo)
-  cfw=$(echo "$info" | jq -r .version)
-  type=$(echo "$info" | jq -r .app)
-  model=$(echo "$info" | jq -r .model) 
+    info=$(curl -qsS -m 5 http://$device/rpc/Shelly.GetInfo)
+    cfw=$(echo "$info" | jq -r .version)
+    type=$(echo "$info" | jq -r .app)
+    model=$(echo "$info" | jq -r .model) 
 
-  if [[ $model != *Shelly* ]]; then
-    case $type in
-      switch1)  
-        model="Shelly1";;
-      switch1pm)
-        model="Shelly1PM";;
-      switch25) 
-        model="Shelly25";;
-      shelly-plug-s)  
-        model="ShellyPlugS";;
-      *) ;;
-    esac
-  fi
-
-  dlurl=$(echo "$release_info" | jq -r '.assets[] | select(.name=="shelly-homekit-'$model'.zip").browser_download_url')
-
-  echo "Host: $device"
-  echo "Type: $type"
-  echo "Model: $model"
-  echo "Current: $cfw"
-  echo "Latest: $lfw"
-  echo "DURL: $dlurl"
-  
-  cfw_V=$(convert_to_integer $cfw)
-  lfw_V=$(convert_to_integer $lfw)
-  
-  if [ $(echo "$lfw_V $cfw_V -p" | dc) == 1 ]; then
-    while true; do
-      read -p "Do you wish to flash $device to firmware version $lfw ?" yn
-      case $yn in
-        [Yy]* )  flash="yes"; break;;
-        [Nn]* ) flash="no";break;;
-        * ) echo "Please answer yes or no.";;
+    if [[ $model != *Shelly* ]]; then
+      case $type in
+        switch1)  
+          model="Shelly1";;
+        switch1pm)
+          model="Shelly1PM";;
+        switch25) 
+          model="Shelly25";;
+        shelly-plug-s)  
+          model="ShellyPlugS";;
+        *) ;;
       esac
-    done
-  else
-    echo "$device DOSE NOT NEED UPDATING..."
-    read -p "Press enter to continue"
-  fi
+    fi
 
-  if [ "$flash" = "yes" ]; then
-    flash $dlurl
-  elif [ "$flash" = "no" ]; then
-    echo "Skipping Flash..."
-    read -p "Press enter to continue"
-  fi
+    dlurl=$(echo "$release_info" | jq -r '.assets[] | select(.name=="shelly-homekit-'$model'.zip").browser_download_url')
 
-done
+    if [ $scriptmode == "flash" ];then 
+      clear
+      echo "Host: $device"
+      echo "Type: $type"
+      echo "Model: $model"
+      echo "Current: $cfw"
+      echo "Latest: $lfw"
+      echo "DURL: $dlurl"
+
+      cfw=2.0.11
+      cfw_V=$(convert_to_integer $cfw)
+      lfw_V=$(convert_to_integer $lfw)
+  
+      if [ $(echo "$lfw_V $cfw_V -p" | dc) == 1 ]; then
+        while true; do
+          read -p "Do you wish to flash $device to firmware version $lfw ? " yn
+          case $yn in
+            [Yy]* )  flash="yes"; break;;
+            [Nn]* ) flash="no";break;;
+            * ) echo "Please answer yes or no.";;
+          esac
+        done
+      else
+        echo "$device DOSE NOT NEED UPDATING..."
+        read -p "Press enter to continue"
+      fi
+
+      if [ "$flash" = "yes" ]; then
+        write_flash $device $lfw $dlurl
+      elif [ "$flash" = "no" ]; then
+        echo "Skipping Flash..."
+        read -p "Press enter to continue"
+      fi
+    else
+      echo "Host: $device"
+      echo "Type: $type"
+      echo "Model: $model"
+      echo "Current: $cfw"
+      echo "Latest: $lfw"
+      echo " "
+    fi
+  done
+}
+
+function help {
+  echo " -f, --flash         Flash the lastest available firmware."
+  echo " -c, --check-only    only check for updates."
+  echo " -h, --help          This help text."
+}
+
+if [ -n "$1" ]; then 
+  if [ $1 == "-h" -o $1 == "--help" ]; then
+    help
+  elif [ $1 == "-f" -o $1 == "--flash" ]; then
+    scriptmode="flash"
+    device-scan
+  elif [ $1 == "-c" -o $1 == "--check-only" ]; then
+    scriptmode="check-only"
+    device-scan
+  fi
+else
+	help
+fi
