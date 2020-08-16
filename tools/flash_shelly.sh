bb33a29faf1cad5003c40df4f25c5f7f60d3025c
Andrew Blackburn <andyblac@icloud.com>
2020-08-16 17:53:33 +0100

    add "Deomid "rojer" Ryabkov" to copyright info.

--- tools/flash_shelly.sh
+++ tools/flash_shelly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#  Copyright (c) 2020 Andrew Blackburn
+#  Copyright (c) 2020 Andrew Blackburn & Deomid "rojer" Ryabkov
 #  All rights reserved
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
