dd8aaefc17cb00054b79d1149d5a77678fb2f93c
Andrew Blackburn <andyblac@icloud.com>
2021-02-14 14:10:27 +0000

    [FlashScript] version 2.3.3

--- tools/flash-shelly.py
+++ tools/flash-shelly.py
@@ -918,7 +918,7 @@ if __name__ == '__main__':
 
   homekit_release_info = None
   stock_release_info = None
-  app_version = "2.3.2"
+  app_version = "2.3.3"
 
   logger.debug(f"OS: {PURPLE}{arch}{NC}")
   logger.debug(f"app_version: {app_version}")
