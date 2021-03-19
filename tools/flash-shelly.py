b6d966fec7e550b17cab8918b815560c3638e881
Andrew Blackburn <andyblac@icloud.com>
2021-03-19 22:41:18 +0000

    version 2.6.2

--- tools/flash-shelly.py
+++ tools/flash-shelly.py
@@ -1138,7 +1138,7 @@ if __name__ == '__main__':
 
   homekit_release_info = None
   stock_release_info = None
-  app_version = "2.6.1"
+  app_version = "2.6.2"
 
   logger.debug(f"OS: {PURPLE}{arch}{NC}")
   logger.debug(f"app_version: {app_version}")
