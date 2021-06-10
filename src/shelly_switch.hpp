326e47fc0854f22d420cc292c3805e701f1848cb
Timo Schilling <timo@schilling.io>
2021-06-10 22:11:22 +0200

    init power chars with nullptr

--- src/shelly_switch.hpp
+++ src/shelly_switch.hpp
@@ -78,8 +78,8 @@ class ShellySwitch : public Component, public mgos::hap::Service {
   void AddPowerMeter(uint16_t *iid);
   void PowerMeterTimerCB();
   mgos::Timer power_timer_;
-  mgos::hap::Characteristic *power_char_;
-  mgos::hap::Characteristic *total_power_char_;
+  mgos::hap::Characteristic *power_char_ = nullptr;
+  mgos::hap::Characteristic *total_power_char_ = nullptr;
   float last_power_ = 0.0f;
   float last_total_power_ = 0.0f;
 };
