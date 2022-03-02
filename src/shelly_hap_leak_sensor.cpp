b0ad60382be76b57d5ae7f6e2e729efd585af1b3
Timo Schilling <timo@schilling.io>
2022-03-03 00:01:40 +0100

    implement leak sensor (#929)

---
+++ src/shelly_hap_leak_sensor.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Shelly-HomeKit Contributors
+ * All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "shelly_hap_leak_sensor.hpp"
+
+namespace shelly {
+namespace hap {
+
+LeakSensor::LeakSensor(int id, Input *in, struct mgos_config_in_sensor *cfg)
+    : SensorBase(id, in, cfg, SHELLY_HAP_IID_BASE_LEAK_SENSOR,
+                 &kHAPServiceType_LeakSensor,
+                 kHAPServiceDebugDescription_LeakSensor) {
+}
+
+LeakSensor::~LeakSensor() {
+}
+
+Component::Type LeakSensor::type() const {
+  return Type::kLeakSensor;
+}
+
+Status LeakSensor::Init() {
+  const Status &st = SensorBase::Init();
+  if (!st.ok()) return st;
+  AddChar(new mgos::hap::UInt8Characteristic(
+      svc_.iid + 2, &kHAPCharacteristicType_LeakDetected, 0, 1, 1,
+      [this](HAPAccessoryServerRef *, const HAPUInt8CharacteristicReadRequest *,
+             uint8_t *value) {
+        *value = !state_;
+        return kHAPError_None;
+      },
+      true /* supports_notification */, nullptr /* write_handler */,
+      kHAPCharacteristicDebugDescription_LeakDetected));
+  return Status::OK();
+}
+
+}  // namespace hap
+}  // namespace shelly
