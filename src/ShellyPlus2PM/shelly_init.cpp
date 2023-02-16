bcf921487dabb0c25e0eed2f19b334d67ca25421
Timo Schilling <timo@schilling.io>
2023-02-16 21:00:47 +0000

    tmp

--- src/ShellyPlus2PM/shelly_init.cpp
+++ src/ShellyPlus2PM/shelly_init.cpp
@@ -15,116 +15,161 @@
  * limitations under the License.
  */
 
-#include "mgos.hpp"
+#include "mgos_ade7953.h"
 
-#include "nvs_flash.h"
-#include "nvs_handle.hpp"
-
-#include "shelly_hap_garage_door_opener.hpp"
 #include "shelly_input_pin.hpp"
 #include "shelly_main.hpp"
-// #include "shelly_pm_bl0937.hpp"
+#include "shelly_pm.hpp"
+#include "shelly_pm_ade7953.hpp"
 #include "shelly_sys_led_btn.hpp"
 #include "shelly_temp_sensor_ntc.hpp"
 
+#include <algorithm>
+
+#include "shelly_hap_garage_door_opener.hpp"
+#include "shelly_hap_input.hpp"
+#include "shelly_hap_window_covering.hpp"
+#include "shelly_main.hpp"
+
 namespace shelly {
 
-// static constexpr const char *kNVSPartitionName = "shelly";
-// static constexpr const char *kNVSNamespace = "shelly";
-// static constexpr const char *kAPowerCoeffNVSKey = "Pm0.apower";
+static struct mgos_ade7953 *s_ade7953 = NULL;
 
-// static StatusOr<float> ReadPowerCoeff() {
-//   esp_err_t err = ESP_OK;
-//   auto fh = nvs::open_nvs_handle_from_partition(
-//       kNVSPartitionName, kNVSNamespace, NVS_READONLY, &err);
-//   if (fh == nullptr) {
-//     return mgos::Errorf(STATUS_NOT_FOUND, "No NVS factory data! err %d", err);
-//   }
-//   size_t size = 0;
-//   err = fh->get_item_size(nvs::ItemType::SZ, kAPowerCoeffNVSKey, size);
-//   if (err != ESP_OK) {
-//     return mgos::Errorf(STATUS_NOT_FOUND, "No power calibration data!");
-//   }
-//   char *buf = (char *) calloc(1, size + 1);  // NUL at the end.
-//   if (buf == nullptr) {
-//     return mgos::Errorf(STATUS_RESOURCE_EXHAUSTED, "Out of memory");
-//   }
-//   mgos::ScopedCPtr buf_owner(buf);
-//   err = fh->get_string(kAPowerCoeffNVSKey, buf, size);
-//   if (err != ESP_OK) {
-//     return mgos::Errorf(STATUS_RESOURCE_EXHAUSTED, "Failed to read key: %d",
-//                         err);
-//   }
-//   float apc = atof(buf);
-//   LOG(LL_DEBUG, ("Factory apower calibration value: %f", apc));
-//   return apc;
-// }
+static Status PowerMeterInit(std::vector<std::unique_ptr<PowerMeter>> *pms) {
+  const struct mgos_config_ade7953 ade7953_cfg = {
+      .voltage_scale = .0000382602,
+      .voltage_offset = -0.068,
+      .current_scale_0 = 0.00000949523,
+      .current_scale_1 = 0.00000949523,
+      .current_offset_0 = -0.017,
+      .current_offset_1 = -0.017,
+      .apower_scale_0 = (1 / 164.0),
+      .apower_scale_1 = (1 / 164.0),
+      .aenergy_scale_0 = (1 / 25240.0),
+      .aenergy_scale_1 = (1 / 25240.0),
+      .voltage_pga_gain = 0,
+      .current_pga_gain_0 = 0,
+      .current_pga_gain_1 = 0,
+  };
+
+  int reset_pin = 33;
+  mgos_gpio_set_mode(reset_pin, MGOS_GPIO_MODE_OUTPUT);
+  mgos_gpio_write(reset_pin, 0);
+  mgos_msleep(100);
+  mgos_gpio_write(reset_pin, 1);
+  mgos_gpio_set_mode(reset_pin, MGOS_GPIO_MODE_INPUT);
+
+
+  s_ade7953 = mgos_ade7953_create(mgos_i2c_get_global(), &ade7953_cfg);
+
+  if (s_ade7953 == nullptr) {
+    LOG(LL_INFO, ("Failed to init ADE7953"));
+    return mgos::Errorf(STATUS_UNAVAILABLE, "Failed to init ADE7953");
+  }
+
+  Status st;
+  std::unique_ptr<PowerMeter> pm1(new ADE7953PowerMeter(1, s_ade7953, 1));
+  if (!(st = pm1->Init()).ok()) return st;
+  std::unique_ptr<PowerMeter> pm2(new ADE7953PowerMeter(2, s_ade7953, 0));
+  if (!(st = pm2->Init()).ok()) return st;
+
+  pms->emplace_back(std::move(pm1));
+  pms->emplace_back(std::move(pm2));
+
+  return Status::OK();
+}
 
 void CreatePeripherals(std::vector<std::unique_ptr<Input>> *inputs,
                        std::vector<std::unique_ptr<Output>> *outputs,
                        std::vector<std::unique_ptr<PowerMeter>> *pms,
                        std::unique_ptr<TempSensor> *sys_temp) {
-  // nvs_flash_init_partition(kNVSPartitionName);
-  outputs->emplace_back(new OutputPin(1, 26, 1));
-  auto *in = new InputPin(1, 4, 1, MGOS_GPIO_PULL_NONE, true);
-  in->AddHandler(std::bind(&HandleInputResetSequence, in, LED_GPIO, _1, _2));
-  in->Init();
-  inputs->emplace_back(in);
+  outputs->emplace_back(new OutputPin(1, 12, 1));
+  outputs->emplace_back(new OutputPin(2, 13, 1));
+  auto *in1 = new InputPin(1, 5, 1, MGOS_GPIO_PULL_NONE, true);
+  in1->AddHandler(std::bind(&HandleInputResetSequence, in1, 4, _1, _2));
+  in1->Init();
+  inputs->emplace_back(in1);
+  auto *in2 = new InputPin(2, 18, 1, MGOS_GPIO_PULL_NONE, false);
+  in2->Init();
+  inputs->emplace_back(in2);
 
-  // // Read factory calibration data but only if the value is default.
-  // // If locally adjusted, do not override.
-  // if (mgos_sys_config_get_bl0937_0_apower_scale() ==
-  //     mgos_sys_config_get_default_bl0937_0_apower_scale()) {
-  //   auto apcs = ReadPowerCoeff();
-  //   if (apcs.ok()) {
-  //     mgos_sys_config_set_bl0937_0_apower_scale(apcs.ValueOrDie());
-  //   } else {
-  //     auto ss = apcs.status().ToString();
-  //     LOG(LL_ERROR, ("Error reading factory calibration data: %s", ss.c_str()));
-  //   }
-  // }
-  // std::unique_ptr<PowerMeter> pm(
-  //     new BL0937PowerMeter(1, 5 /* CF */, 18 /* CF1 */, 23 /* SEL */, 2,
-  //                          mgos_sys_config_get_bl0937_0_apower_scale()));
-  // const Status &st = pm->Init();
-  // if (st.ok()) {
-  //   pms->emplace_back(std::move(pm));
-  // } else {
-  //   const std::string &s = st.ToString();
-  //   LOG(LL_ERROR, ("PM init failed: %s", s.c_str()));
-  // }
-  // sys_temp->reset(new TempSensorSDNT1608X103F3950(32, 3.3f, 10000.0f));
+  const Status &st = PowerMeterInit(pms);
+  if (!st.ok()) {
+    const std::string &s = st.ToString();
+    LOG(LL_INFO, ("Failed to init ADE7953: %s", s.c_str()));
+  }
 
-  // InitSysLED(LED_GPIO, LED_ON);
-  // InitSysBtn(BTN_GPIO, BTN_DOWN);
+  sys_temp->reset(new TempSensorSDNT1608X103F3950(35, 3.3f, 10000.0f));
 }
 
 void CreateComponents(std::vector<std::unique_ptr<Component>> *comps,
                       std::vector<std::unique_ptr<mgos::hap::Accessory>> *accs,
                       HAPAccessoryServerRef *svr) {
-  // if (mgos_sys_config_get_shelly_mode() == 2) {
-  //   // Garage door opener mode.
-  //   auto *gdo_cfg = (struct mgos_config_gdo *) mgos_sys_config_get_gdo1();
-  //   std::unique_ptr<hap::GarageDoorOpener> gdo(
-  //       new hap::GarageDoorOpener(1, FindInput(1), nullptr /* in_open */,
-  //                                 FindOutput(1), FindOutput(1), gdo_cfg));
-  //   if (gdo == nullptr) return;
-  //   auto st = gdo->Init();
-  //   if (!st.ok()) {
-  //     LOG(LL_ERROR, ("GDO init failed: %s", st.ToString().c_str()));
-  //     return;
-  //   }
-  //   gdo->set_primary(true);
-  //   mgos::hap::Accessory *pri_acc = (*accs)[0].get();
-  //   pri_acc->SetCategory(kHAPAccessoryCategory_GarageDoorOpeners);
-  //   pri_acc->AddService(gdo.get());
-  //   comps->emplace_back(std::move(gdo));
-  //   return;
-  // }
-  // // Single switch with non-detached input = only one accessory.
-  // bool to_pri_acc = (mgos_sys_config_get_sw1_in_mode() != 3);
-  // CreateHAPSwitch(1, mgos_sys_config_get_sw1(), mgos_sys_config_get_in1(),
-  //                 comps, accs, svr, to_pri_acc);
+  // Roller-shutter mode.
+  if (mgos_sys_config_get_shelly_mode() == 1) {
+    const int id = 1;
+    auto *wc_cfg = (struct mgos_config_wc *) mgos_sys_config_get_wc1();
+    auto im = static_cast<hap::WindowCovering::InMode>(wc_cfg->in_mode);
+    Input *in1 = FindInput(1), *in2 = FindInput(2);
+    std::unique_ptr<hap::WindowCovering> wc(
+        new hap::WindowCovering(id, in1, in2, FindOutput(1), FindOutput(2),
+                                FindPM(1), FindPM(2), wc_cfg));
+    if (wc == nullptr || !wc->Init().ok()) {
+      return;
+    }
+    wc->set_primary(true);
+    switch (im) {
+      case hap::WindowCovering::InMode::kSeparateMomentary:
+      case hap::WindowCovering::InMode::kSeparateToggle: {
+        // Single accessory with a single primary service.
+        mgos::hap::Accessory *pri_acc = (*accs)[0].get();
+        pri_acc->SetCategory(kHAPAccessoryCategory_WindowCoverings);
+        pri_acc->AddService(wc.get());
+        break;
+      }
+      case hap::WindowCovering::InMode::kSingle:
+      case hap::WindowCovering::InMode::kDetached: {
+        std::unique_ptr<mgos::hap::Accessory> acc(
+            new mgos::hap::Accessory(SHELLY_HAP_AID_BASE_WINDOW_COVERING + id,
+                                     kHAPAccessoryCategory_BridgedAccessory,
+                                     wc_cfg->name, GetIdentifyCB(), svr));
+        acc->AddHAPService(&mgos_hap_accessory_information_service);
+        acc->AddService(wc.get());
+        accs->push_back(std::move(acc));
+        if (im == hap::WindowCovering::InMode::kDetached) {
+          hap::CreateHAPInput(1, mgos_sys_config_get_in1(), comps, accs, svr);
+          hap::CreateHAPInput(2, mgos_sys_config_get_in2(), comps, accs, svr);
+        } else if (wc_cfg->swap_inputs) {
+          hap::CreateHAPInput(1, mgos_sys_config_get_in1(), comps, accs, svr);
+        } else {
+          hap::CreateHAPInput(2, mgos_sys_config_get_in2(), comps, accs, svr);
+        }
+        break;
+      }
+    }
+    comps->emplace(comps->begin(), std::move(wc));
+    return;
+  }
+  // Garage door opener mode.
+  if (mgos_sys_config_get_shelly_mode() == 2) {
+    auto *gdo_cfg = (struct mgos_config_gdo *) mgos_sys_config_get_gdo1();
+    std::unique_ptr<hap::GarageDoorOpener> gdo(new hap::GarageDoorOpener(
+        1, FindInput(1), FindInput(2), FindOutput(1), FindOutput(2), gdo_cfg));
+    if (gdo == nullptr || !gdo->Init().ok()) {
+      return;
+    }
+    gdo->set_primary(true);
+    mgos::hap::Accessory *pri_acc = (*accs)[0].get();
+    pri_acc->SetCategory(kHAPAccessoryCategory_GarageDoorOpeners);
+    pri_acc->AddService(gdo.get());
+    comps->emplace_back(std::move(gdo));
+    return;
+  }
+
+  CreateHAPSwitch(1, mgos_sys_config_get_sw1(), mgos_sys_config_get_in1(),
+                  comps, accs, svr, false /* to_pri_acc */);
+  CreateHAPSwitch(2, mgos_sys_config_get_sw2(), mgos_sys_config_get_in2(),
+                  comps, accs, svr, false /* to_pri_acc */);
 }
 
 }  // namespace shelly
