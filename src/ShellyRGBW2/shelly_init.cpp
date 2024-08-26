2edf89a6f3b94803c403c0f542700a17dd065fe8
Markus Kirberg <markus@flobby.net>
2024-08-26 07:06:13 +0200

    new pinning

--- src/ShellyRGBW2/shelly_init.cpp
+++ src/ShellyRGBW2/shelly_init.cpp
@@ -40,10 +40,26 @@ void CreatePeripherals(std::vector<std::unique_ptr<Input>> *inputs,
   in->Init();
   inputs->emplace_back(in);
 
-  // TODO: add more inputs for +RGBWPM
+// TODO: add more inputs for +RGBWPM
+#ifdef GPIO_I2
+  in = new InputPin(2, GPIO_I2, 1, MGOS_GPIO_PULL_NONE, true);
+  in->AddHandler(std::bind(&HandleInputResetSequence, in, 0, _1, _2));
+  in->Init();
+  inputs->emplace_back(in);
 
-  // InitSysLED(LED_GPIO, LED_ON);
-  // InitSysBtn(BTN_GPIO, BTN_DOWN);
+  in = new InputPin(3, GPIO_I3, 1, MGOS_GPIO_PULL_NONE, true);
+  in->AddHandler(std::bind(&HandleInputResetSequence, in, 0, _1, _2));
+  in->Init();
+  inputs->emplace_back(in);
+
+  in = new InputPin(4, GPIO_I4, 1, MGOS_GPIO_PULL_NONE, true);
+  in->AddHandler(std::bind(&HandleInputResetSequence, in, 0, _1, _2));
+  in->Init();
+  inputs->emplace_back(in);
+#endif
+
+  InitSysLED(LED_GPIO, LED_ON);
+  InitSysBtn(BTN_GPIO, BTN_DOWN);
 }
 
 void CreateComponents(std::vector<std::unique_ptr<Component>> *comps,
